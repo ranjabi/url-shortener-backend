13c90a0a2493aa8b638c133e902bc279d149490a
Fikri Ranjabi <71055612+ranjabi@users.noreply.github.com>
2024-12-28 22:27:49 +0700

    feat: bean object

---
+++ src/main/java/com/ranjabi/urlshortener/Addresses.java
@@ -0,0 +1,19 @@
+package com.ranjabi.urlshortener;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Addresses {
+
+    @Bean
+    public String address1() {
+        return "Green Street, 102";
+    }
+
+    @Bean
+    public String address2() {
+        return "Apple Street, 15";
+    }
+
+}
--- src/main/java/com/ranjabi/urlshortener/UrlShortenerApplication.java
+++ src/main/java/com/ranjabi/urlshortener/UrlShortenerApplication.java
@@ -1,7 +1,10 @@
 package com.ranjabi.urlshortener;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UrlShortenerApplication {
@@ -10,4 +13,15 @@ public class UrlShortenerApplication {
 		SpringApplication.run(UrlShortenerApplication.class, args);
 	}
 
+	@Bean
+    public Customer customer(@Autowired @Qualifier("address2") String address) {
+        return new Customer("Clara Foster", address);
+    }
+
+	@Bean
+	// beans are initialized during the application startup
+	public Customer temporary(@Autowired Customer customer) {
+		System.out.println(customer);
+		return customer;
+	}
 }
