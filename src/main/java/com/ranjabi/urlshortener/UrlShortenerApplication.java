3659a80b808b3494f48522f9f8780e11e4e05279
Fikri Ranjabi <71055612+ranjabi@users.noreply.github.com>
2024-12-28 22:24:29 +0700

    chore: rename class

--- src/main/java/com/ranjabi/urlshortener/UrlShortenerApplication.java
+++ src/main/java/com/ranjabi/urlshortener/UrlShortenerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UrlSshortenerApplication {
+public class UrlShortenerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UrlSshortenerApplication.class, args);
+		SpringApplication.run(UrlShortenerApplication.class, args);
 	}
 
 }
