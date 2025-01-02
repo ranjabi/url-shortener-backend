144c4107ad236160854c333e9e765fe6267d8028
Fikri Ranjabi <71055612+ranjabi@users.noreply.github.com>
2025-01-02 07:23:12 +0700

    feat: swagger ui with springdoc for auth

--- src/main/java/com/ranjabi/urlshortener/authentication/AuthController.java
+++ src/main/java/com/ranjabi/urlshortener/authentication/AuthController.java
@@ -12,8 +12,15 @@ import com.ranjabi.urlshortener.dto.response.AuthResponse;
 import com.ranjabi.urlshortener.dto.response.SuccessResponse;
 import com.ranjabi.urlshortener.user.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
-@RequestMapping(path="/auth")
+@RequestMapping(path = "/auth")
+@Tag(name = "auth", description = "Authentication")
 public class AuthController {
     private final AuthService authService;
     private final UserService userService;
@@ -23,16 +30,26 @@ public class AuthController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Register new user")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "409", description = "Username already exists", content = @Content) })
+    // TODO 400 bad request
     @PostMapping("/register")
     public ResponseEntity<SuccessResponse<Void>> register(@RequestBody AuthRequest request) {
         userService.saveUser(request.getUsername(), request.getPassword());
-        
+
         return ResponseEntity.status(HttpStatus.CREATED).body(SuccessResponse.ofMessage("Account has been created"));
     }
 
+    @Operation(summary = "Login")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "401", description = "Username/password is wrong", content = @Content) })
+    // TODO 400 bad request
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+    public ResponseEntity<SuccessResponse<AuthResponse>> login(@RequestBody AuthRequest request) {
         AuthResponse response = authService.authenticate(request.getUsername(), request.getPassword());
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(SuccessResponse.of("", response));
     }
 }
